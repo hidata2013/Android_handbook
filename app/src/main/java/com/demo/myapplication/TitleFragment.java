3f262f6ea3db61c6e1e13f09cc504b5e037d21e1
stan <stan.guo@zoom.us>
2018-09-26 17:36:44 +0800

    TextViewFragment add same codes.

--- app/src/main/java/com/demo/myapplication/TitleFragment.java
+++ app/src/main/java/com/demo/myapplication/TitleFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,7 +15,7 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TitleFragment extends Fragment {
+public class TitleFragment extends Fragment implements TitleAdapter.OnItemClick {
 
     RecyclerView recyclerView;
     TitleAdapter adapter;
@@ -29,7 +31,9 @@ public class TitleFragment extends Fragment {
         titleList.add("TextView");
         titleList.add("TextView");
         adapter.setTitleList(titleList);
+        adapter.setOnItemClick(this);
         recyclerView.setAdapter(adapter);
+
     }
 
     @Override
@@ -43,20 +47,57 @@ public class TitleFragment extends Fragment {
         return inflater.inflate(R.layout.title_list, null);
     }
 
-    class ItemClick implements TitleAdapter.OnItemClick {
 
-        @Override
-        public void onItemClick(int position, View view) {
-            String titleName = null;
-            if (view.getTag() instanceof String) {
-                titleName = (String) view.getTag();
-            }
-            if (titleName == null || titleName.length() == 0) {
-                return;
-            }
-//            switch (titleName){
-//                case
-//            }
+    @Override
+    public void onItemClick(int position, View view) {
+        String titleName = null;
+        if (view.getTag() instanceof String) {
+            titleName = (String) view.getTag();
         }
+        if (titleName == null || titleName.length() == 0) {
+            return;
+        }
+        switch (titleName) {
+            case "title":
+
+        }
+    }
+
+
+    public void add(Fragment fragment, int id, String tag) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //优先检查，fragment是否存在，避免重叠
+        Fragment tempFragment = (Fragment) fragmentManager.findFragmentByTag(tag);
+//        if (fragment.isAdded()) {
+//            addOrShowFragment(fragmentTransaction, fragment, id, tag);
+//        } else {
+//            if (currentFragment != null && currentFragment.isAdded()) {
+//                fragmentTransaction.hide(currentFragment).add(id, fragment, tag).commit();
+//            } else {
+//                fragmentTransaction.add(id, fragment, tag).commit();
+//            }
+//            currentFragment = fragment;
+//        }
+        fragmentTransaction.add(id, fragment, tag).commit();
+//        fragmentTransaction.replace();
+    }
+
+    /**
+     * 添加或者显示 fragment
+     *
+     * @param fragment
+     */
+    private void addOrShowFragment(FragmentTransaction transaction, Fragment fragment, int id, String tag) {
+//        if (currentFragment == fragment)
+//            return;
+//        if (!fragment.isAdded()) { // 如果当前fragment未被添加，则添加到Fragment管理器中
+//            transaction.hide(currentFragment).add(id, fragment, tag).commit();
+//        } else {
+//            transaction.hide(currentFragment).show(fragment).commit();
+//        }
+//        currentFragment.setUserVisibleHint(false);
+//        currentFragment = fragment;
+//        currentFragment.setUserVisibleHint(true);
     }
 }
--- app/src/main/java/com/demo/myapplication/ViewActivity.java
+++ app/src/main/java/com/demo/myapplication/ViewActivity.java
@@ -3,14 +3,43 @@ package com.demo.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewActivity extends FragmentActivity {
+
+    Fragment fragmentLeft;
+    Fragment fragmentRight;
+    Button btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_fragment_page);
+        btn = findViewById(R.id.sw);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentLeft = new TitleFragment();
+                fragmentRight = new ContentFragment();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, fragmentLeft, "right").commit();
+            }
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentLeft = new TitleFragment();
+        fragmentRight = new ContentFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, fragmentLeft, "left");
+        // commit already called , 需要统一一起commit
+        fragmentTransaction.add(R.id.container, fragmentRight, "right").commit();
+//        fragmentTransaction.replace(R.id.right_pane, fragmentRight, fragmentRight.getTag()).commit();
+
     }
 
 }
